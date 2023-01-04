825d7e7c902dd7f0c9643cac3bf5c18908876ca6
jtk-dev <jtk.develop@gmail.com>
2023-01-05 06:01:42 +0900

    dev

--- src/main/java/com/example/springbootstarter/SpringbootStarterApplication.java
+++ src/main/java/com/example/springbootstarter/SpringbootStarterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootStarterApplication {
 
 	public static void main(String[] args) {
-		// 주석 -dev
+		// 주석 -dev1
 		SpringApplication.run(SpringbootStarterApplication.class, args);
 	}
 }
